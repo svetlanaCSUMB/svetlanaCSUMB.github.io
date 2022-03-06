11a6dcc966cd1abd34f2b5ceeecf29918cce7114
svetlanaCSUMB <svendborgotter@gmail.com>
2022-03-06 15:37:43 -0800

    articulate

---
+++ articulate/story_content/5pGmBDUmlQj_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.60.26947.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.310%0D%0ALet\u0027s%20have%20some%20practice.%20Drag%20the%20items%20to%20appropriate%20boxes.%20%0D%0A%0D%0A"}')
