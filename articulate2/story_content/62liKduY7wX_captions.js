71e4acce1890411f332cf1dab26b6ab72970396d
svetlanaCSUMB <svendborgotter@gmail.com>
2022-03-06 18:50:30 -0800

    new

---
+++ articulate2/story_content/62liKduY7wX_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.60.26947.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.814%0D%0AMeet%20Angela.%20%20Let%E2%80%99s%20help%20her%20to%20reduce%20stress%20and%20anxiety.%0D%0A%0D%0A"}')
---
+++ articulate2/story_content/65877dTDOyy_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.60.26947.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.754%0D%0AHover%20over%20the%20markers%20to%20check%20if%20these%20products%20help%20Angela%20to%20reduce%20anxiety.%20%0D%0A%0D%0A"}')
